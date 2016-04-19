404bc0fcafd6944b0f3db0e70bd6a57b552a84be
andela-rekemezie <rowland.ekemezie@andela.com>
2016-04-19 19:10:50 +0100

    Update the use case for using Refs in an application

--- App.js
+++ App.js
@@ -5,45 +5,53 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      great: 'This is a constructor thing'
+      red: 0,
+      green: 0,
+      blue: 0
     };
     this.update = this.update.bind(this);
   }
 
-  update(e) {
-    this.setState({great: e.target.value});
+  update() {
+    this.setState({
+      red: ReactDOM.findDOMNode(this.refs.red.refs.inp).value,
+      green: ReactDOM.findDOMNode(this.refs.green.refs.inp).value,
+      blue: ReactDOM.findDOMNode(this.refs.blue.refs.inp).value
+    });
   }
 
   render() {
-    let cat = this.cat;
+   // let cat = this.cat;
     return (
       <div>
-        <h3>{this.props.text}, {cat} </h3>
-        <Widget great = {this.state.great}
-        update={this.update} counter={this.state.counter} click={this.click} />
-        <Widget great = {this.state.great}
-         update={this.update} counter={this.state.counter} click={this.click} />
-        <Widget great = {this.state.great}
-         update={this.update} counter={this.state.counter} click={this.click} />
-        <Widget great = {this.state.great}
-         update={this.update} counter={this.state.counter} click={this.click} />
+        <Slider ref="red" update={this.update} />
+        {this.state.red}
+        <br />
+        <Slider ref="green" update={this.update} />
+        {this.state.green}
+        <br />
+        <Slider ref="blue" update={this.update} />
+        {this.state.blue}
       </div>
     );
   }
 }
 
-// A new component to illustrate the composite example
-// using a stateless function
-// Refs will not work with stateless function
-const Widget = (props) => {
-  return (
-    <div>
-      <input type="text" onChange={props.update}/>
-      <h1>{props.great}</h1>
-  </div>
-  );
-};
-
+//  Refs is a way to reference the instance of our component in an application
+class Slider extends React.Component {
+  render() {
+    return (
+      <div>
+        <input ref="inp"
+        type="range"
+        onChange={this.props.update}
+        min="0"
+        max="255" />
+      <h3> Testing our features for refs </h3>
+      </div>
+    );
+  }
+}
 App.propTypes = {
   cat: React.PropTypes.number.isRequired,
   text: React.PropTypes.string.isRequired
@@ -54,11 +62,5 @@ App.defaultProps = {
 };
 
 ReactDOM.render(
-  <App cat={5} />,
-  document.getElementById('app')
+  <App cat={5} />, document.getElementById('app')
 );
-// A stateless function component
-// This means that this compoinent will not have a state
-// const App = () => <div> Hello world </div>;
-
-// export default App;
