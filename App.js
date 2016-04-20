f552ce25eda6efa0bce36cf3c5acd8da4eebba0e
andela-rekemezie <rowland.ekemezie@andela.com>
2016-04-20 02:18:30 +0100

    Added the a skeletal view of the react component life cycle

--- App.js
+++ App.js
@@ -2,33 +2,58 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {val: 0};
+    this.update = this.update.bind(this);
+  }
+
+  update() {
+    this.setState({val: this.state.val + 1});
+  }
+
+  componentWillMount() {
+    console.log('Mounting');
+    this.setState({m: 2});
+  }
+  componentDidMount() {
+    console.log('Mounted');
+    this.inc = setInterval(this.update, 500);
+    // console.log(ReactDOM.findDOMNode(this));
+  }
+  render() {
+    console.log('Rendering');
+    return (
+      <div>
+        <button onClick={this.update}>{this.state.val * this.state.m}</button>
+      </div>
+    );
+  }
+  componentWillUnmount() {
+    console.log('Unmounted');
+    clearInterval(this.inc);
+  }
+}
+
+export class Wrapper extends React.Component {
+  constructor() {
+    super();
+  }
+  mount() {
+    ReactDOM.render(<App />, document.getElementById('a'));
+  }
+  unmount() {
+    ReactDOM.unmountComponentAtNode(document.getElementById('a'));
+  }
   render() {
     return (
       <div>
-        <Button>I<Heart />React</Button>
+        <button onClick={this.mount.bind(this)}>Mount</button>
+         <button onClick={this.unmount.bind(this)}>Unmount</button>
+         <div id="a"></div>
       </div>
     );
   }
 }
 
-class Button extends React.Component {
-  render() {
-    return (
-      <div>
-        <button>{this.props.children}</button>
-      </div>
-    );
-  }
-}
-
-const Heart = () => {
-  return (
-    <div>
-      <span className="glyphicon glyphicon-heart"></span>
-    </div>
-  );
-};
-
-ReactDOM.render(
-  <App />, document.getElementById('app')
-);
+ReactDOM.render(<Wrapper />, document.getElementById('app'));
